81381463d47ca6e297d910343bbc98e271868a1a
Ali Osman <aliosmankorkmaz61@hotmail.com>
2023-04-26 12:02:15 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,9 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu  bir atm projesidir.");
+        //version1
+        System.out.println("java is easy");
+        //version2
+
     }
 }
